9769dbe4cc7324ecf40c84cd652fc7fac0a35b4a
tako0910 <tako.oct0910@gmail.com>
2013-09-28 00:14:40 +0900

    Revert "Update battery battery by m7wlj CM sources"
    This reverts commit f9d11e38d1e78d1f29106f6079fbad371e45eee3.

--- arch/arm/mach-msm/board-deluxe_j.c
+++ arch/arm/mach-msm/board-deluxe_j.c
@@ -1031,16 +1031,12 @@ static int pm8921_is_wireless_charger(void)
 }
 #endif
 
-static int critical_alarm_voltage_mv[] = {3000, 3100, 3200, 3400};
-
 static struct htc_battery_platform_data htc_battery_pdev_data = {
 	.guage_driver = 0,
 	.chg_limit_active_mask = HTC_BATT_CHG_LIMIT_BIT_TALK |
-								HTC_BATT_CHG_LIMIT_BIT_NAVI |
-								HTC_BATT_CHG_LIMIT_BIT_THRML,
+								HTC_BATT_CHG_LIMIT_BIT_NAVI,
 	.critical_low_voltage_mv = 3100,
-	.critical_alarm_vol_ptr = critical_alarm_voltage_mv,
-	.critical_alarm_vol_cols = sizeof(critical_alarm_voltage_mv) / sizeof(int),
+	.critical_alarm_voltage_mv = 3000,
 	.overload_vol_thr_mv = 4000,
 	.overload_curr_thr_ma = 0,
 
@@ -1052,10 +1048,8 @@ static struct htc_battery_platform_data htc_battery_pdev_data = {
 	.icharger.enable_5v_output = smb349_enable_5v_output,
 #else
 	.icharger.name = "pm8921",
-	.icharger.sw_safetytimer = 0,
 	.icharger.set_limit_charge_enable = pm8921_limit_charge_enable,
 	.icharger.get_attr_text = pm8921_charger_get_attr_text,
-	.icharger.enable_5v_output = NULL,
 #endif
 	.icharger.get_charging_source = pm8921_get_charging_source,
 	.icharger.get_charging_enabled = pm8921_get_charging_enabled,
@@ -1066,14 +1060,11 @@ static struct htc_battery_platform_data htc_battery_pdev_data = {
 	.icharger.is_ovp = pm8921_is_charger_ovp,
 	.icharger.is_batt_temp_fault_disable_chg =
 						pm8921_is_batt_temp_fault_disable_chg,
-	//.icharger.is_vbus_unstable = pm8921_is_vbus_unstable,
 	.icharger.is_under_rating = pm8921_is_pwrsrc_under_rating,
 	.icharger.charger_change_notifier_register =
 						cable_detect_register_notifier,
 	.icharger.dump_all = pm8921_dump_all,
-
-
-
+	
 	.igauge.name = "pm8921",
 	.igauge.get_battery_voltage = pm8921_get_batt_voltage,
 	.igauge.get_battery_current = pm8921_bms_get_batt_current,
@@ -1090,7 +1081,6 @@ static struct htc_battery_platform_data htc_battery_pdev_data = {
 	.igauge.set_lower_voltage_alarm_threshold =
 						pm8xxx_batt_lower_alarm_threshold_set,
 };
-
 static struct platform_device htc_battery_pdev = {
 	.name = "htc_battery",
 	.id = -1,
